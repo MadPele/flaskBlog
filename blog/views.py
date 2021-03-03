9f9695e62b7df3d6489e26af47846495c60ffce4
Andrzej Derela <andrzejderela@gmail.com>
2021-03-03 18:02:39 +0100

    Add templates, navigation and css

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,28 @@
 from blog import app
+from flask import render_template, url_for
+
+posts = [
+    {
+        'author': 'Pele',
+        'title': '1st post',
+        'content': '1st post content',
+        'date_posted': 'February 6, 2021'
+    },
+    {
+        'author': 'MadPele',
+        'title': '2nd post',
+        'content': '2nd post content',
+        'date_posted': 'February 6, 2021'
+    }
+]
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World2!'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
